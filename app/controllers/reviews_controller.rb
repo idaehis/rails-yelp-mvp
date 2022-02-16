8416fd49c7441a41ef3d27a114ddd2ee28e9e56d
Ida Schwarzkopf <ida.schwarzkopf@ha-es.de>
2022-02-16 13:02:26 +0100

    it works, but it is not styled

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,15 +1,23 @@
 class ReviewsController < ApplicationController
   def new
     @review = Review.new
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 
   def create
-
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@review)
+    else
+      render :new
+    end
   end
 
   private
 
-  def restaurant_params
-    params.require(:restaurant).permit(:rating, :content)
+  def review_params
+    params.require(:review).permit(:rating, :content)
   end
 end
